9660727c65388f024a773fd94c5ff6c90fac4e15
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-04-27 20:18:56 -0700

    creating graph

--- GraphProject/Task2.java
+++ GraphProject/Task2.java
@@ -2,6 +2,18 @@ import java.util.*;
 
 class Graph<E> 
 {
-
+    private Map<E, Integer> vertexToIndex;
+	private E[] indexToVertex;
+    private int[][] adjacencyMatrix;
+	 
+     @SuppressWarnings("unchecked")
+	public Graph(E[] vertices) 
+    {
+    	 adjacencyMatrix = new int[vertices.length][vertices.length];
+    	 vertexToIndex = new HashMap<>();
+    	 indexToVertex = (E[])new Object[vertices.length];
+    	 addVertex(vertices);
+     }
+     
 
 }
